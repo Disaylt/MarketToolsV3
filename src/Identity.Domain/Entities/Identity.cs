8bde3f8a0b4c8eae9b398f23c12efd4836affccf
Disaylt <disofban@gmail.com>
2024-10-31 10:34:22 +0300

    init: add identity

---
+++ src/Identity.Domain/Entities/Identity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Domain.Entities
+{
+    public class Identity : IdentityUser
+    {
+        public DateTime CreateDate { get; } = DateTime.UtcNow;
+    }
+}
