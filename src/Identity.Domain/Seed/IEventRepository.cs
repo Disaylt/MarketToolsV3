94b8f9170360789138299832af75a6735195e5ee
Disaylt <disofban@gmail.com>
2024-10-31 10:43:39 +0300

    init: add event repository

---
+++ src/Identity.Domain/Seed/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Seed
+{
+    internal interface IEventRepository
+    {
+        public IReadOnlyCollection<INotification> Notifications { get; }
+        Task PublishAllAsync();
+        void RemoveNotification(INotification notification);
+        void AddNotification(INotification notification);
+        void ClearNotifications();
+    }
+}
