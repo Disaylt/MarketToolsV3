a1010da08b89e5183a43c621be6ffefe3f151365
Disaylt <disofban@gmail.com>
2024-10-31 10:15:01 +0300

    init: add tests

---
+++ tests/MarketToolsV3.Users.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MarketToolsV3.Users.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
