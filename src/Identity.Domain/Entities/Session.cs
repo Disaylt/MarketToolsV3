4c65cb37c409c5f333ad393f5f19abd56a374fad
Disaylt <disofban@gmail.com>
2024-10-31 10:37:40 +0300

    init: create session

---
+++ src/Identity.Domain/Entities/Session.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Entities
+{
+    public class Session
+    {
+        public string Id { get; private set; }
+        public string? Token { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Created { get; private set; } = DateTime.UtcNow;
+        public DateTime Updated { get; set; } = DateTime.UtcNow;
+        public Identity Identity { get; private set; } = null!;
+        public string IdentityId { get; private set; }
+        public string UserAgent { get; set; }
+
+        protected Session()
+        {
+            IsActive = true;
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public Session(string identityId, string userAgent) : this()
+        {
+            IdentityId = identityId;
+            UserAgent = userAgent;
+        }
+    }
+}
