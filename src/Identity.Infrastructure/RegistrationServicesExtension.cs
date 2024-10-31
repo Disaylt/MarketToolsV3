8076d08b1de20b2ed02f63382f4d5f43b679d76d
Disaylt <disofban@gmail.com>
2024-10-31 12:15:56 +0300

    feat: add configuration for identity

--- src/Identity.Infrastructure/RegistrationServicesExtension.cs
+++ src/Identity.Infrastructure/RegistrationServicesExtension.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Identity.Domain.Entities;
 using Identity.Domain.Seed;
+using Identity.Infrastructure.Database;
 using Identity.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Identity.Infrastructure
 {
     public static class RegistrationServicesExtension
     {
-        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection collection)
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection collection, IConfiguration configuration)
         {
+            IConfigurationSection connectionSection = configuration.GetSection("ConnectionStrings");
+            string dbConnection = connectionSection.GetValue<string>("identities") ?? string.Empty;
+
             collection.AddScoped<IEventRepository, EventRepository>();
+            collection.AddNpgsql<IdentityDbContext>(dbConnection);
+
+            collection.AddIdentityCore<IdentityPerson>(options =>
+                {
+                    options.User.RequireUniqueEmail = true;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                })
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
 
             return collection;
         }
