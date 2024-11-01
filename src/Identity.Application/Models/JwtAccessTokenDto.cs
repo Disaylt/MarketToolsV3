9dbe4570e2c0c587da326b964ee05feadfd9d1c3
Disaylt <disofban@gmail.com>
2024-11-01 18:17:13 +0300

    init: create models

---
+++ src/Identity.Application/Models/JwtAccessTokenDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Models
+{
+    public record JwtAccessTokenDto : IToken
+    {
+        public required string UserId { get; init; }
+        public List<string> Roles { get; } = new List<string>();
+    }
+}
---
+++ src/Identity.Application/Models/SessionStatusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Models
+{
+    public record SessionStatusDto
+    {
+        public required string Id { get; init; }
+        public bool IsActive { get; set; }
+    }
+}
