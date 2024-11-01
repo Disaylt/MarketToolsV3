e55e7970d9f4e695de55acdd6cb550903074f8e5
Disaylt <disofban@gmail.com>
2024-11-01 16:21:45 +0300

    init: reate indetity person service abstraction

---
+++ src/Identity.Application/Services/IIdentityPersonService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Identity.Domain.Entities;
+
+namespace Identity.Application.Services
+{
+    public interface IIdentityPersonService
+    {
+        Task<IdentityPerson> AddAsync(IdentityPerson user, string password);
+        Task<IdentityPerson?> FindByEmailAsync(string email);
+        Task<bool> CheckPassword(IdentityPerson user, string password);
+    }
+}
