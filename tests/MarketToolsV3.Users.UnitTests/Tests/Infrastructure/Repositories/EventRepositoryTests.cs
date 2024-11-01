439517c54f69ce40bdc32792f8c6f659c9e47ffb
Disaylt <disofban@gmail.com>
2024-11-01 09:56:47 +0300

    init: add tests for publish

--- tests/MarketToolsV3.Users.UnitTests/Tests/Infrastructure/Repositories/EventRepositoryTests.cs
+++ tests/MarketToolsV3.Users.UnitTests/Tests/Infrastructure/Repositories/EventRepositoryTests.cs
@@ -12,12 +12,12 @@ namespace MarketToolsV3.Users.UnitTests.Tests.Infrastructure.Repositories
 {
     public class EventRepositoryTests
     {
-        private IMediator _mediator;
+        private Mock<IMediator> _mediator;
 
         [SetUp]
         public void Setup()
         {
-            _mediator = new Mock<IMediator>().Object;
+            _mediator = new Mock<IMediator>();
         }
 
         [TestCase(1)]
@@ -25,7 +25,7 @@ namespace MarketToolsV3.Users.UnitTests.Tests.Infrastructure.Repositories
         [TestCase(5)]
         public void ClearNotifications_AddNotifications_Expected0(int quantity)
         {
-            IEventRepository eventRepository = new EventRepository(_mediator);
+            IEventRepository eventRepository = new EventRepository(_mediator.Object);
 
             for (int i = 0; i < quantity; i++)
             {
@@ -43,7 +43,7 @@ namespace MarketToolsV3.Users.UnitTests.Tests.Infrastructure.Repositories
         [TestCase(5)]
         public void AddNotifications_AddQuantity_ExpectedQuantityFromCase(int quantity)
         {
-            IEventRepository eventRepository = new EventRepository(_mediator);
+            IEventRepository eventRepository = new EventRepository(_mediator.Object);
 
             for (int i = 0; i < quantity; i++)
             {
@@ -57,11 +57,47 @@ namespace MarketToolsV3.Users.UnitTests.Tests.Infrastructure.Repositories
         [TestCaseSource(nameof(CreateNotificationForRemoveNotification))]
         public void RemoveNotification_AddNotification_ExpectedNotContainsCase(INotification notification)
         {
-            IEventRepository eventRepository = new EventRepository(_mediator);
+            IEventRepository eventRepository = new EventRepository(_mediator.Object);
             eventRepository.AddNotification(notification);
             eventRepository.RemoveNotification(notification);
 
             Assert.IsFalse(eventRepository.Notifications.Contains(notification));
+        }
+
+        [TestCase(4)]
+        [TestCase(8)]
+        [TestCase(10)]
+        public async Task PublishAllAsync_AddQuantityNotifications_ExpectedCallPublishCaseCount(int quantity)
+        {
+            IEventRepository eventRepository = new EventRepository(_mediator.Object);
+
+            for (int i = 0; i < quantity; i++)
+            {
+                INotification notification = new Mock<INotification>().Object;
+                eventRepository.AddNotification(notification);
+            }
+
+            await eventRepository.PublishAllAsync();
+
+            _mediator.Verify(m => m.Publish(It.IsAny<INotification>(), default), Times.Exactly(quantity));
+        }
+
+        [TestCase(4)]
+        [TestCase(8)]
+        [TestCase(10)]
+        public async Task PublishAllAsync_AddQuantityNotifications_ExpectedClearNotifications(int quantity)
+        {
+            IEventRepository eventRepository = new EventRepository(_mediator.Object);
+
+            for (int i = 0; i < quantity; i++)
+            {
+                INotification notification = new Mock<INotification>().Object;
+                eventRepository.AddNotification(notification);
+            }
+
+            await eventRepository.PublishAllAsync();
+
+            Assert.That(eventRepository.Notifications.Count, Is.EqualTo(0));
         }
 
         private static IEnumerable<TestCaseData> CreateNotificationForRemoveNotification()
