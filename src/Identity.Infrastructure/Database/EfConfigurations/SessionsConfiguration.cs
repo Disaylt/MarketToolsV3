13bfcfd1b614a2258caa99b4b28cb6d2a7c0dce6
Disaylt <disofban@gmail.com>
2024-11-01 23:02:21 +0300

    init: add sessiong configuration

---
+++ src/Identity.Infrastructure/Database/EfConfigurations/SessionsConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Database.EfConfigurations
+{
+    internal class SessionsConfiguration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.ToTable("sessions");
+
+            builder.HasKey(e => e.Id);
+
+            builder.HasIndex(e => e.IdentityId);
+
+            builder.Property(e => e.Token)
+                .HasMaxLength(1000);
+
+            builder.Property(e => e.UserAgent)
+                .HasMaxLength(1000);
+
+            builder.HasOne(x => x.Identity)
+                .WithMany(x => x.Sessions)
+                .HasForeignKey(e => e.IdentityId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
