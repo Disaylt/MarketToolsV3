3ab950aabbcf2467fe63f0b051ba6af0d99724c0
Disaylt <disofban@gmail.com>
2024-11-01 23:06:00 +0300

    init: add sessiong service abstaraction

---
+++ src/Identity.Application/Services/ISessionService.cs
@@ -0,0 +1,19 @@
+﻿using Identity.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Services
+{
+    public interface ISessionService
+    {
+        Task<Session> AddAsync(Session session, CancellationToken cancellationToken);
+
+        Task UpdateAsync(Session session, string token, string userAgent = "Unknown",
+            CancellationToken cancellationToken = default);
+        Task<IEnumerable<Session>> GetActiveSessionsAsync(string userId, CancellationToken cancellationToken);
+        Task DeleteAsync(string id, CancellationToken cancellationToken);
+    }
+}
