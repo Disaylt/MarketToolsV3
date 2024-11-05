401b53b1e0fd36e36ba9bfee5b9b419af6957f93
Disaylt <disofban@gmail.com>
2024-11-05 17:43:39 +0300

    init: add test

--- tests/MarketToolsV3.Users.UnitTests/Tests/Infrastructure/Services/Tokens/JwtAccessTokenServiceTests.cs
+++ tests/MarketToolsV3.Users.UnitTests/Tests/Infrastructure/Services/Tokens/JwtAccessTokenServiceTests.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace MarketToolsV3.Users.UnitTests.Tests.Infrastructure.Services.Tokens
 {
@@ -24,6 +26,26 @@ namespace MarketToolsV3.Users.UnitTests.Tests.Infrastructure.Services.Tokens
             _claimsServiceMock = new Mock<IClaimsService<JwtAccessTokenDto>>();
             _jwtTokenServiceMock = new Mock<IJwtTokenService>();
             _optionsMock = new Mock<IOptions<ServiceConfiguration>>();
+        }
+
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        public void Read_ReturnUserIdInToken(string userId)
+        {
+            JwtAccessTokenService jwtAccessTokenService = new JwtAccessTokenService(_claimsServiceMock.Object,
+                _jwtTokenServiceMock.Object,
+                _optionsMock.Object);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+                claims: new List<Claim> { new Claim(ClaimTypes.NameIdentifier, userId) });
+
+            _jwtTokenServiceMock.Setup(x => x.ReadJwtToken(It.IsAny<string>()))
+                .Returns(jwtSecurityToken);
+
+            JwtAccessTokenDto jwtAccessToken = jwtAccessTokenService.Read(It.IsAny<string>());
+
+            Assert.That(jwtAccessToken.UserId, Is.EqualTo(userId));
         }
     }
 }
