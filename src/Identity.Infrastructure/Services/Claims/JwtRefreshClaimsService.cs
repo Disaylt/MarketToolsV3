e6ab1e5b3264ccb07280791313a70fc1628e9203
Disaylt <disofban@gmail.com>
2024-11-05 23:26:33 +0300

    init: create tests

--- src/Identity.Infrastructure/Services/Claims/JwtRefreshClaimsService.cs
+++ src/Identity.Infrastructure/Services/Claims/JwtRefreshClaimsService.cs
@@ -11,22 +11,22 @@ using System.Threading.Tasks;
 
 namespace Identity.Infrastructure.Services.Claims
 {
-    internal class JwtRefreshClaimsService : IClaimsService<JwtRefreshTokenDto>
+    public class JwtRefreshClaimsService : IClaimsService<JwtRefreshTokenDto>
     {
         public IEnumerable<Claim> Create(JwtRefreshTokenDto details)
         {
-            Claim jti = new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString());
-            Claim iat = new Claim(JwtRegisteredClaimNames.Iat,
+            Claim jti = new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString());
+            Claim iat = new(JwtRegisteredClaimNames.Iat,
                 EpochTime.GetIntDate(DateTime.UtcNow).ToString(CultureInfo.InvariantCulture),
                 ClaimValueTypes.Integer64);
-            Claim sessionId = new Claim(ClaimTypes.Sid, details.Id);
+            Claim sessionId = new(ClaimTypes.Sid, details.Id);
 
-            List<Claim> claims = new List<Claim>
-            {
+            List<Claim> claims =
+            [
                 jti,
                 iat,
                 sessionId
-            };
+            ];
 
             return claims;
         }
