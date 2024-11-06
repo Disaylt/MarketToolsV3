8d009f2fde8c7124ab42b3be642de08b8bcd09cf
Disaylt <disofban@gmail.com>
2024-11-06 13:15:23 +0300

    init: create tests

--- tests/MarketToolsV3.Users.UnitTests/Tests/Applications/Commands/LoginCommandHandlerTests.cs
+++ tests/MarketToolsV3.Users.UnitTests/Tests/Applications/Commands/LoginCommandHandlerTests.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Castle.Core.Logging;
 using Identity.Application.Commands;
 using Identity.Application.Models;
 using Identity.Application.Services;
+using Identity.Domain.Entities;
+using Identity.Domain.Seed;
 using MarketToolsV3.Users.UnitTests.Source;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -20,6 +23,7 @@ namespace MarketToolsV3.Users.UnitTests.Tests.Applications.Commands
         private Mock<ITokenService<JwtAccessTokenDto>> _accessTokenServiceMock;
         private Mock<ITokenService<JwtRefreshTokenDto>> _refreshTokenServiceMock;
         private ILogger<LoginCommandHandler> _logger;
+        private LoginCommand _command;
 
         [SetUp]
         public void Setup()
@@ -29,6 +33,58 @@ namespace MarketToolsV3.Users.UnitTests.Tests.Applications.Commands
             _accessTokenServiceMock = new Mock<ITokenService<JwtAccessTokenDto>>();
             _refreshTokenServiceMock = new Mock<ITokenService<JwtRefreshTokenDto>>();
             _logger = TestLogging.Create<LoginCommandHandler>();
+            _command = new LoginCommand
+            {
+                Email = "email-1",
+                Password = "password-1",
+                UserAgent = "user-agent-1"
+            };
+        }
+
+        [Test]
+        public async Task Handle_WhenUserNull_ReturnException()
+        {
+            LoginCommandHandler commandHandler = new LoginCommandHandler(
+                _identityPersonServiceMock.Object,
+                _sessionServiceMock.Object,
+                _logger,
+                _accessTokenServiceMock.Object,
+                _refreshTokenServiceMock.Object);
+
+            _identityPersonServiceMock
+                .Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(It.IsAny<IdentityPerson>());
+
+            RootServiceException ex = Assert.CatchAsync<RootServiceException>(() =>
+                commandHandler.Handle(_command, It.IsAny<CancellationToken>()));
+
+            Assert.That(ex.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+
+        }
+
+        [Test]
+        public async Task Handle_WhenBadCheckPassword_ReturnException()
+        {
+            LoginCommandHandler commandHandler = new LoginCommandHandler(
+                _identityPersonServiceMock.Object,
+                _sessionServiceMock.Object,
+                _logger,
+                _accessTokenServiceMock.Object,
+                _refreshTokenServiceMock.Object);
+
+            _identityPersonServiceMock
+                .Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(new IdentityPerson());
+
+            _identityPersonServiceMock
+                .Setup(x=> x.CheckPassword(It.IsAny<IdentityPerson>(), It.IsAny<string>()))
+                .ReturnsAsync(false);
+
+            RootServiceException ex = Assert.CatchAsync<RootServiceException>(() =>
+                commandHandler.Handle(_command, It.IsAny<CancellationToken>()));
+
+            Assert.That(ex.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+
         }
     }
 }
