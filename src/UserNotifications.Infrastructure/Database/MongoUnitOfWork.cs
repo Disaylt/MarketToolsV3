95adff318b5515c3e51d5f47bae96503204956d7
Disaylt <disofban@gmail.com>
2024-11-08 17:17:56 +0300

    init: implement unitofwork

---
+++ src/UserNotifications.Infrastructure/Database/MongoUnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserNotifications.Domain.Seed;
+
+namespace UserNotifications.Infrastructure.Database
+{
+    public class MongoUnitOfWork(IClientSessionHandleContext sessionContext)
+        : IUnitOfWork
+    {
+        private readonly IClientSessionHandle _sessionHandle = sessionContext.Session;
+
+        public bool HasTransaction => _sessionHandle.IsInTransaction;
+
+        public Task BeginTransactionAsync()
+        {
+            if (HasTransaction == false)
+            {
+                _sessionHandle.StartTransaction();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            await _sessionHandle.CommitTransactionAsync();
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            await _sessionHandle.AbortTransactionAsync();
+        }
+
+        public Task SaveEntitiesAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
