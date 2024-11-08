9b9b078bdc2ca62c6f24775e854b357b798fc0e6
Disaylt <disofban@gmail.com>
2024-11-08 14:21:10 +0300

    feat: fill repository interface

--- src/UserNotifications.Domain/Seed/IRepository.cs
+++ src/UserNotifications.Domain/Seed/IRepository.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserNotifications.Domain.Entities;
 
 namespace UserNotifications.Domain.Seed
 {
-    public interface IRepository
+    public interface IRepository<T> where T : Entity
     {
+        IUnitOfWork UnitOfWork { get; }
+        Task<T> FindByIdAsync(string id, CancellationToken cancellationToken);
+        Task InsertAsync(T entity, CancellationToken cancellationToken);
     }
 }
