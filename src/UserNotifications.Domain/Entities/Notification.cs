d52643102a8cea5612384d0245bce349f4a96ad8
Disaylt <disofban@gmail.com>
2024-11-08 13:42:45 +0300

    init: create notification

---
+++ src/UserNotifications.Domain/Entities/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNotifications.Domain.Entities
+{
+    public class Notification
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string UserId { get; set; }
+        public required string Message { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public bool IsRead { get; set; }
+    }
+}
