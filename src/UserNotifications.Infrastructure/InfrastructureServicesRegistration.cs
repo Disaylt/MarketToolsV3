2aed311d9526b64f3ee456dba2f928a66b89d854
Disaylt <disofban@gmail.com>
2024-11-11 11:59:12 +0300

    feat: change logging behavoir

---
+++ src/UserNotifications.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserNotifications.Infrastructure.Database;
+
+namespace UserNotifications.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IClientSessionHandleContext, ClientSessionHandleContext>();
+            services.AddSingleton<IDatabaseClient<IMongoClient>, DatabaseClient>();
+
+            return services;
+        }
+    }
+}
