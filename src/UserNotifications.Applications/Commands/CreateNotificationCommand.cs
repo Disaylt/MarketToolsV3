398884b4fbcad7b24de6723329362ae81f1e4a64
Disaylt <disofban@gmail.com>
2024-11-11 12:53:31 +0300

    init: create command

---
+++ src/UserNotifications.Applications/Commands/CreateNotificationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace UserNotifications.Applications.Commands
+{
+    public class CreateNotificationCommand : ICommand<Unit>
+    {
+        public required string UserId { get; set; }
+        public required string Message { get; set; }
+    }
+}
