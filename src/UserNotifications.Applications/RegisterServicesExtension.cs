0de80709c49a0eb742796c3a1d3b5425ff03c753
Disaylt <disofban@gmail.com>
2024-11-11 13:26:38 +0300

    feat: add register services

--- src/UserNotifications.Applications/RegisterServicesExtension.cs
+++ src/UserNotifications.Applications/RegisterServicesExtension.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UserNotifications.Applications.Behaviors;
 
 namespace UserNotifications.Applications
 {
     public static class RegisterServicesExtension
     {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
 
+                cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                cfg.AddOpenBehavior(typeof(TransactionBehavior<,>));
+            });
+
+            return serviceCollection;
+        }
     }
 }
