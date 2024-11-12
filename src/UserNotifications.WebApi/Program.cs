dd319cb4c64d3f676afad96143e23ec2fc3fdfe1
Disaylt <disofban@gmail.com>
2024-11-12 14:51:50 +0300

    feat: add configuration

--- src/UserNotifications.WebApi/Program.cs
+++ src/UserNotifications.WebApi/Program.cs
@@ -1,8 +1,21 @@
+using MarketToolsV3.ConfigurationManager;
+using UserNotifications.Applications;
+using UserNotifications.Domain.Seed;
+using UserNotifications.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+await builder.Configuration.LoadConfigurationAsync();
+
+IConfigurationSection serviceSection = builder.Configuration.GetSection("UserNotifications");
+builder.Services.AddOptions<ServiceConfiguration>()
+    .Bind(serviceSection);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationLayer()
+    .AddInfrastructureServices(serviceSection);
 
 var app = builder.Build();
 
