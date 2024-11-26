8212670c67350971ca8a17f81426f20b99811bc0
Disaylt <disofban@gmail.com>
2024-11-26 18:41:59 +0300

    init: create event

---
+++ src/IntegrationEvents.Contract/Identity/CreatedIdentityIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEvents.Contract.Identity
+{
+    public record CreatedIdentityIntegrationEvent : BaseIntegrationEvent
+    {
+        public required string IdentityId { get; init; }
+    }
+}
