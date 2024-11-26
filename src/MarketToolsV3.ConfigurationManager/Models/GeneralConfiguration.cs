6dba883ca7ffe4cbdda06223dace487041c98aa5
Disaylt <disofban@gmail.com>
2024-11-27 00:09:31 +0300

    init: add rabbitmq configuration

--- src/MarketToolsV3.ConfigurationManager/Models/GeneralConfiguration.cs
+++ src/MarketToolsV3.ConfigurationManager/Models/GeneralConfiguration.cs
@@ -9,8 +9,11 @@ namespace MarketToolsV3.ConfigurationManager.Models
     public class GeneralConfiguration
     {
         public string AuthSecret { get; set; } = string.Empty;
-        public string ElasticUrl { get; set; } = string.Empty;
+        public string ElasticConnection { get; set; } = string.Empty;
         public string ElasticUser { get; set; } = string.Empty;
         public string ElasticPassword { get; set; } = string.Empty;
+        public string RabbitMqConnection { get; set; } = string.Empty;
+        public string RabbitMqLogin { get; set; } = string.Empty;
+        public string RabbitMqPassword{ get; set; } = string.Empty;
     }
 }
