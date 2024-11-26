e0cf7f0c4d2b9319fdfcc1d8ec2351fff6b8df31
Disaylt <disofban@gmail.com>
2024-11-26 15:59:31 +0300

    init: add event

---
+++ IntegrationEvents.Contract/Identity/UserCreatedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEvents.Contract.Identity
+{
+    public record UserCreatedIntegrationEvent
+    {
+        public Guid CommandId { get; init; }
+        public required string UserId { get; init; }
+    }
+}
