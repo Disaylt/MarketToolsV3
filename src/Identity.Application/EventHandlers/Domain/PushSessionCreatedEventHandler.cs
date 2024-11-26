1f0611768140106fb7295d70a86783461977d701
Disaylt <disofban@gmail.com>
2024-11-26 19:09:59 +0300

    init: create event handler

---
+++ src/Identity.Application/EventHandlers/Domain/PushSessionCreatedEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Identity.Domain.Events;
+using IntegrationEvents.Contract.Identity;
+using MassTransit;
+using MediatR;
+
+namespace Identity.Application.EventHandlers.Domain
+{
+    public class PushSessionCreatedEventHandler(IBus bus)
+        : INotificationHandler<SessionCreated>
+    {
+        public async Task Handle(SessionCreated notification, CancellationToken cancellationToken)
+        {
+            SessionCreatedIntegrationEvent integrationMessage = new SessionCreatedIntegrationEvent
+            {
+                SessionId = notification.Session.Id
+            };
+
+            await bus.Publish(integrationMessage, cancellationToken);
+        }
+    }
+}
