4d255419726c945e736016dab967a4d67fa1e63a
Disaylt <disofban@gmail.com>
2024-11-27 23:07:31 +0300

    init: create message broker config

---
+++ src/MarketToolsV3.ConfigurationManager/Models/MessageBrokerConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketToolsV3.ConfigurationManager.Models
+{
+    public record MessageBrokerConfig
+    {
+        public string RabbitMqConnection { get; init; } = string.Empty;
+        public string RabbitMqLogin { get; init; } = string.Empty;
+        public string RabbitMqPassword { get; init; } = string.Empty;
+    }
+}
