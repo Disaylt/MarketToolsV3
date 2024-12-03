7294e837a12e923e4a5c73371ec398a55014cc47
Disaylt <disofban@gmail.com>
2024-12-03 14:31:29 +0300

    feat: add services to app host

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,4 +1,8 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var identityService = builder.AddProject<Identity_WebApi>("IdentityService");
+var userNotificationProcessor = builder.AddProject<UserNotifications_Processor>("UserNotificationProcessor");
 
 builder.Build().Run();
