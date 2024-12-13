c88ac48f0f909c55418fe058160b223d65c2e702
Disaylt <disofban@gmail.com>
2024-12-14 01:14:14 +0300

    init: create owner entity

---
+++ src/WB.Seller.Api.Companies.Domain/Entities/Owner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Seller.Api.Companies.Domain.Entities
+{
+    public class Owner
+    {
+        public string Id { get; private set; } = null!;
+
+        protected Owner()
+        {
+
+        }
+
+        public Owner(string id)
+        {
+            Id = id;
+        }
+    }
+}
