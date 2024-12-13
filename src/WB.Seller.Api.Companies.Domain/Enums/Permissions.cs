e2255ea55a5169bca1589d1f4f40d906c18c50b9
Disaylt <disofban@gmail.com>
2024-12-14 01:37:44 +0300

    init: create enum permission

---
+++ src/WB.Seller.Api.Companies.Domain/Enums/Permissions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Seller.Api.Companies.Domain.Enums
+{
+    public enum Permissions
+    {
+        [Description("Недоступно")]
+        None,
+
+        [Description("Только чтение")]
+        OnlyRead,
+
+        [Description("Чтение и запись")]
+        ReadAndWrite
+    }
+}
