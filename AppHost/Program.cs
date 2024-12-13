fb800f8da4974f52ced94b2e31a158b14fdd706d
Disaylt <disofban@gmail.com>
2024-12-13 22:33:17 +0300

    feat: delete variables

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -5,12 +5,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 var configTypeParameter = builder.AddParameter("ConfigType");
 var jsonBasePathParameter = builder.AddParameter("JsonBasePath");
 
-var identityService = builder
+builder
     .AddProject<Identity_WebApi>("IdentityService")
     .WithEnvironment("ConfigType", configTypeParameter)
     .WithEnvironment("JsonBasePath", jsonBasePathParameter);
 
-var userNotificationProcessor = builder
+builder
     .AddProject<UserNotifications_Processor>("UserNotificationProcessor")
     .WithEnvironment("ConfigType", configTypeParameter)
     .WithEnvironment("JsonBasePath", jsonBasePathParameter);
