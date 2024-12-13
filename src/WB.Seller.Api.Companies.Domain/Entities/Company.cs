653af946f5c46edfe0342525c7c8a6350542b5e1
Disaylt <disofban@gmail.com>
2024-12-14 01:19:58 +0300

    init: create company entity

---
+++ src/WB.Seller.Api.Companies.Domain/Entities/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Seller.Api.Companies.Domain.Entities
+{
+    public class Company
+    {
+        public string Name { get; private set; }
+        public string? Token { get; private set; }
+
+        protected Company()
+        {
+            Name = null!;
+        }
+
+        public Company(string name, string? token) : this()
+        {
+            Name = name;
+            Token = token;
+        }
+    }
+}
