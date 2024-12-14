e04c90c000d7dd730c60bfe6b54e8695d4100062
Disaylt <disofban@gmail.com>
2024-12-14 13:25:56 +0300

    init: create subscription

---
+++ src/WB.Seller.Api.Companies.Domain/Entities/Subscription.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Seller.Api.Companies.Domain.Entities
+{
+    public class Subscription
+    {
+        public string SubscriberId { get; private set; }
+        public Subscriber Subscriber { get; private set; }
+
+        public int CompanyId { get; private set; }
+        public Company Company { get; private set; }
+
+        protected Subscription()
+        {
+            SubscriberId = null!;
+            Subscriber = null!;
+            Company = null!;
+        }
+
+        public Subscription(string subscriberId, int companyId) : this()
+        {
+            SubscriberId = subscriberId;
+            CompanyId = companyId;
+        }
+
+        public Subscription(Subscriber subscriber, Company company) : this()
+        {
+            Subscriber = subscriber;
+            Company = company;
+        }
+    }
+}
