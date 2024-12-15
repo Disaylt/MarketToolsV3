bb7b099ab3e7570dee487df858fcb5bbd6eb19a1
Disaylt <disofban@gmail.com>
2024-12-15 18:29:03 +0300

    feat: create company configuration

---
+++ src/WB.Seller.Api.Companies.Infrastructure/Database/EfConfigurations/CompanyConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Seller.Api.Companies.Domain.Entities;
+
+namespace WB.Seller.Api.Companies.Infrastructure.Database.EfConfigurations
+{
+    internal class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.ToTable("companies");
+
+            builder.HasKey(c => c.Id);
+
+            builder.HasIndex(c => c.OwnerId);
+
+            builder.Property(c => c.Token)
+                .HasMaxLength(2000);
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(100);
+        }
+    }
+}
