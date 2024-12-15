ab8b65ad2867049aa870246ac9fda4963ce529f3
Disaylt <disofban@gmail.com>
2024-12-15 19:36:44 +0300

    feat: add extension with db connection

--- src/WB.Seller.Api.Companies.WebApi/Program.cs
+++ src/WB.Seller.Api.Companies.WebApi/Program.cs
@@ -1,8 +1,15 @@
+using MarketToolsV3.ConfigurationManager;
+using MarketToolsV3.ConfigurationManager.Abstraction;
+using WB.Seller.Api.Companies.Domain.Seed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 string serviceName = "wb-seller-api-companies";
 
 builder.AddServiceDefaults();
+
+ConfigurationServiceFactory configurationServiceFactory = new(builder.Configuration);
+ITypingConfigManager<ServiceConfiguration> serviceConfigManager = configurationServiceFactory.CreateFromService<ServiceConfiguration>(serviceName);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
