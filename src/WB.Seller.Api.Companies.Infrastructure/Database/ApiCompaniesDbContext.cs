eff6c2cd06e5bc19c30e072bb9a2d83d524a966f
Disaylt <disofban@gmail.com>
2024-12-15 18:12:43 +0300

    init: create db context

---
+++ src/WB.Seller.Api.Companies.Infrastructure/Database/ApiCompaniesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WB.Seller.Api.Companies.Domain.Entities;
+
+namespace WB.Seller.Api.Companies.Infrastructure.Database
+{
+    public class ApiCompaniesDbContext(DbContextOptions<ApiCompaniesDbContext> options) : DbContext(options)
+    {
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<Owner> Owners { get; set; } = null!;
+        public DbSet<Permission> Permissions { get; set; } = null!;
+        public DbSet<Subscriber> Subscribers { get; set; } = null!;
+        public DbSet<Subscription> Subscriptions { get; set; } = null!;
+    }
+}
