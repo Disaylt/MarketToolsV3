1ae5dda16aac32b9ed1bd722637ea039fa35a634
Disaylt <disofban@gmail.com>
2024-12-16 21:58:03 +0300

    init: create new migration

--- src/WB.Seller.Api.Companies.Infrastructure/Migrations/20241216185426_ChangePermissionIndexTypeSubscription.Designer.cs
+++ src/WB.Seller.Api.Companies.Infrastructure/Migrations/20241216185426_ChangePermissionIndexTypeSubscription.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WB.Seller.Api.Companies.Infrastructure.Database;
@@ -11,9 +12,11 @@ using WB.Seller.Api.Companies.Infrastructure.Database;
 namespace WB.Seller.Api.Companies.Infrastructure.Migrations
 {
     [DbContext(typeof(ApiCompaniesDbContext))]
-    partial class ApiCompaniesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241216185426_ChangePermissionIndexTypeSubscription")]
+    partial class ChangePermissionIndexTypeSubscription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/WB.Seller.Api.Companies.Infrastructure/Migrations/20241216185426_ChangePermissionIndexTypeSubscription.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WB.Seller.Api.Companies.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePermissionIndexTypeSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_permissions_Type_Status_SubscriptionId",
+                table: "permissions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permissions_Type_SubscriptionId",
+                table: "permissions",
+                columns: new[] { "Type", "SubscriptionId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_permissions_Type_SubscriptionId",
+                table: "permissions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permissions_Type_Status_SubscriptionId",
+                table: "permissions",
+                columns: new[] { "Type", "Status", "SubscriptionId" },
+                unique: true);
+        }
+    }
+}
