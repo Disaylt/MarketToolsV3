e9385adafefaadc136d7f602c0ee279dc7264f99
Disaylt <disofban@gmail.com>
2025-01-07 18:47:43 +0300

    feat: add session service

--- src/Identity.GrpcService/Program.cs
+++ src/Identity.GrpcService/Program.cs
@@ -37,6 +37,7 @@ var app = builder.Build();
 app.MapDefaultEndpoints();
 
 app.MapGrpcService<AuthService>();
+app.MapGrpcService<SessionService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ src/Identity.GrpcService/Services/SessionService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using Identity.Application.Queries;
+using MediatR;
+using Proto.Contract.Identity;
+
+namespace Identity.GrpcService.Services;
+
+public class SessionService(IMediator mediator)
+    : Session.SessionBase
+{
+    public override async Task<SessionActiveStatusReply> GetActiveStatus(SessionInfoRequest request, ServerCallContext context)
+    {
+        CheckSessionActiveStatusQuery command = new CheckSessionActiveStatusQuery
+        {
+            RefreshToken = request.Token
+        };
+
+        bool isActive = await mediator.Send(command);
+
+        return new SessionActiveStatusReply
+        {
+            IsActive = isActive
+        };
+    }
+}
