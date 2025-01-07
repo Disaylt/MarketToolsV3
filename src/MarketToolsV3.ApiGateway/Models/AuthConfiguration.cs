dec6635840976945bc3cff8d58a3b62ab1143599
Disaylt <disofban@gmail.com>
2025-01-07 16:37:16 +0300

    feat: create cookie adapter

---
+++ src/MarketToolsV3.ApiGateway/Models/AuthConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace MarketToolsV3.ApiGateway.Models
+{
+    public class AuthConfiguration
+    {
+        public string AccessTokenName { get; set; } = "access-token";
+        public string RefreshTokenName { get; set; } = "refresh-token";
+    }
+}
