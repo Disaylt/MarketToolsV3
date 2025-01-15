690064e2c805425cbe02d6c10ec90646ec0d5fa5
Uladzislau Krupski <uladzislau.krupski@alreadymedia.com>
2025-01-15 15:58:21 +0300

    refactor: change names

---
+++ src/MarketToolsV3.DbMigrations.Service/Extensions/WbSellerCompaniesMigrationExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarketToolsV3.ConfigurationManager;
+
+namespace MarketToolsV3.DbMigrations.Service.Extensions
+{
+    internal static class WbSellerCompaniesMigrationExtension
+    {
+        public static async Task AddWbSellerCompaniesMigration(this IHostApplicationBuilder builder)
+        {
+            ConfigurationServiceFactory configurationServiceFactory = new(builder.Configuration);
+        }
+    }
+}
