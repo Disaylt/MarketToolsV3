c5a93e4ebccce90fc5e014f24e7615afd3e6a32a
Disaylt <disofban@gmail.com>
2025-01-16 22:59:55 +0300

    feat: add logging to migration service

--- src/MarketToolsV3.DbMigrations.Service/Program.cs
+++ src/MarketToolsV3.DbMigrations.Service/Program.cs
@@ -1,12 +1,7 @@
-using Google.Protobuf.WellKnownTypes;
-using MarketToolsV3.DbMigrations.Service.Attributes;
 using MarketToolsV3.DbMigrations.Service.Extensions;
-using MarketToolsV3.DbMigrations.Service.Models;
 using MarketToolsV3.DbMigrations.Service.Services.Implementation;
 using MarketToolsV3.DbMigrations.Service.Services.Interfaces;
-using MarketToolsV3.DbMigrations.Service.Workers;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+using MarketToolsV3.DbMigrations.Service;
 
 var builder = Host.CreateApplicationBuilder(args);
 
