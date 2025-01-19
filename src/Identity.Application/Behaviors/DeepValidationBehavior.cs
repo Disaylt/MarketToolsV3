aa0d2340a3df1d0a8b5686a9172e0ac09304d993
Disaylt <disofban@gmail.com>
2025-01-20 01:01:20 +0300

    feat: create deep validation

---
+++ src/Identity.Application/Behaviors/DeepValidationBehavior.cs
@@ -0,0 +1,26 @@
+﻿using Identity.Application.DeepValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Behaviors
+{
+    internal class DeepValidationBehavior<TRequest, TResponse>(IEnumerable<IDeepValidator<TRequest>> deepValidators)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+
+            foreach (var deepValidator in deepValidators)
+            {
+                await deepValidator.Handle(request);
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ src/Identity.Application/DeepValidation/SessionDeepValidator.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Application.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.DeepValidation
+{
+    public interface ISessionDeepValidationRequest
+    {
+        string Id { get; set; }
+        string UserId { get; set; }
+    }
+
+    internal class SessionDeepValidator : IDeepValidator<ISessionDeepValidationRequest>
+    {
+        public Task Handle(ISessionDeepValidationRequest request)
+        {
+           
+            return Task.CompletedTask;
+        }
+    }
+}
