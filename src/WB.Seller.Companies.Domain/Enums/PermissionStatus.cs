8d986968ff832dc4e6bad7bcf4dbcdb38d5e5386
Disaylt <disofban@gmail.com>
2025-01-21 00:25:55 +0300

    init: create entities

---
+++ src/WB.Seller.Companies.Domain/Enums/PermissionStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Seller.Companies.Domain.Enums
+{
+    public enum PermissionStatus
+    {
+        [Description("Недоступно")]
+        None,
+
+        [Description("Только чтение")]
+        OnlyRead,
+
+        [Description("Чтение и запись")]
+        ReadAndWrite
+    }
+}
