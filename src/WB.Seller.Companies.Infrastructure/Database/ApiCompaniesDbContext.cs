7923b1b90426cb54cb8f2a0b4651ba051e342a8c
Disaylt <disofban@gmail.com>
2025-01-21 00:59:14 +0300

    feat: delete old projects

---
+++ src/WB.Seller.Companies.Infrastructure/Database/ApiCompaniesDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Seller.Companies.Domain.Entities;
+using WB.Seller.Companies.Infrastructure.Database.EfConfigurations;
+
+namespace WB.Seller.Companies.Infrastructure.Database
+{
+    public class ApiCompaniesDbContext(DbContextOptions<ApiCompaniesDbContext> options) : DbContext(options)
+    {
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<Owner> Owners { get; set; } = null!;
+        public DbSet<Permission> Permissions { get; set; } = null!;
+        public DbSet<Subscriber> Subscribers { get; set; } = null!;
+        public DbSet<Subscription> Subscriptions { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new OwnerConfiguration());
+            modelBuilder.ApplyConfiguration(new SubscriberConfiguration());
+            modelBuilder.ApplyConfiguration(new SubscriptionConfiguration());
+            modelBuilder.ApplyConfiguration(new PermissionConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
