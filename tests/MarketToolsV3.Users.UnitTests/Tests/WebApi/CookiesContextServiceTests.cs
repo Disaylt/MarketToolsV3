7d8f39f6d7d6cfc6ba3b6c7d5131e47286890d6b
Spurdo <Spurdo@REVISION-PC>
2025-02-03 20:45:57 +0300

    tests fixes

--- tests/MarketToolsV3.Users.UnitTests/Tests/WebApi/CookiesContextServiceTests.cs
+++ tests/MarketToolsV3.Users.UnitTests/Tests/WebApi/CookiesContextServiceTests.cs
@@ -12,7 +12,6 @@ namespace MarketToolsV3.Users.UnitTests.Tests.WebApi
     public class CookiesContextServiceTests
     {
         private Mock<IHttpContextAccessor> _httpContextAccessorMock;
-        private CookiesContextService _service;
         private Mock<IResponseCookies> _responseCookiesMock;
         private Mock<IHeaderDictionary> _responseHeadersMock;
 
@@ -22,25 +21,22 @@ namespace MarketToolsV3.Users.UnitTests.Tests.WebApi
             _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
             _responseCookiesMock = new Mock<IResponseCookies>();
             _responseHeadersMock = new Mock<IHeaderDictionary>();
+        }
 
-
+        [Test]
+        public void AddAccessToken_ShouldAddAccessTokenCookie()
+        {
             var responseMock = new Mock<HttpResponse>();
             responseMock.SetupGet(r => r.Cookies).Returns(_responseCookiesMock.Object);
-            responseMock.SetupGet(r => r.Headers).Returns(_responseHeadersMock.Object);
-
 
             var httpContextMock = new Mock<HttpContext>();
             httpContextMock.SetupGet(c => c.Response).Returns(responseMock.Object);
             _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
 
-
             var authConfigOptions = Options.Create(new AuthConfig());
-            _service = new CookiesContextService(_httpContextAccessorMock.Object, authConfigOptions);
-        }
+            var service = new CookiesContextService(_httpContextAccessorMock.Object, authConfigOptions);
 
-        public void AddAccessToken_ShouldAddAccessTokenCookie()
-        {
-            _service.AddAccessToken(It.IsAny<string>());
+            service.AddAccessToken(It.IsAny<string>());
 
             _responseCookiesMock.Verify(
                 x => x.Append(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CookieOptions>()),
@@ -51,12 +47,20 @@ namespace MarketToolsV3.Users.UnitTests.Tests.WebApi
         [Test]
         public void AddSessionToken_ShouldAddSessionTokenCookie()
         {
-            const string token = "test-session-token";
+            var responseMock = new Mock<HttpResponse>();
+            responseMock.SetupGet(r => r.Cookies).Returns(_responseCookiesMock.Object);
 
-            _service.AddSessionToken(token);
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.SetupGet(c => c.Response).Returns(responseMock.Object);
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+
+            var authConfigOptions = Options.Create(new AuthConfig());
+            var service = new CookiesContextService(_httpContextAccessorMock.Object, authConfigOptions);
+
+            service.AddSessionToken("test-session-token");
 
             _responseCookiesMock.Verify(
-                x => x.Append("refresh-token", token, It.IsAny<CookieOptions>()),
+                x => x.Append("refresh-token", "test-session-token", It.IsAny<CookieOptions>()),
                 Times.Once,
                 "Refresh token was not added correctly.");
         }
@@ -64,7 +68,17 @@ namespace MarketToolsV3.Users.UnitTests.Tests.WebApi
         [Test]
         public void DeleteAccessToken_ShouldRemoveAccessTokenCookie()
         {
-            _service.DeleteAccessToken();
+            var responseMock = new Mock<HttpResponse>();
+            responseMock.SetupGet(r => r.Cookies).Returns(_responseCookiesMock.Object);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.SetupGet(c => c.Response).Returns(responseMock.Object);
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+
+            var authConfigOptions = Options.Create(new AuthConfig());
+            var service = new CookiesContextService(_httpContextAccessorMock.Object, authConfigOptions);
+
+            service.DeleteAccessToken();
 
             _responseCookiesMock.Verify(
                 x => x.Delete("access-token"),
@@ -75,7 +89,17 @@ namespace MarketToolsV3.Users.UnitTests.Tests.WebApi
         [Test]
         public void DeleteSessionToken_ShouldRemoveSessionTokenCookie()
         {
-            _service.DeleteSessionToken();
+            var responseMock = new Mock<HttpResponse>();
+            responseMock.SetupGet(r => r.Cookies).Returns(_responseCookiesMock.Object);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.SetupGet(c => c.Response).Returns(responseMock.Object);
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+
+            var authConfigOptions = Options.Create(new AuthConfig());
+            var service = new CookiesContextService(_httpContextAccessorMock.Object, authConfigOptions);
+
+            service.DeleteSessionToken();
 
             _responseCookiesMock.Verify(
                 x => x.Delete("refresh-token"),
@@ -86,7 +110,17 @@ namespace MarketToolsV3.Users.UnitTests.Tests.WebApi
         [Test]
         public void MarkAsNew_ShouldAddCorrectHeader()
         {
-            _service.MarkAsNew();
+            var responseMock = new Mock<HttpResponse>();
+            responseMock.SetupGet(r => r.Headers).Returns(_responseHeadersMock.Object);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.SetupGet(c => c.Response).Returns(responseMock.Object);
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+
+            var authConfigOptions = Options.Create(new AuthConfig());
+            var service = new CookiesContextService(_httpContextAccessorMock.Object, authConfigOptions);
+
+            service.MarkAsNew();
 
             _responseHeadersMock.VerifySet(
                 h => h["mp-cookie-action"] = "new",
