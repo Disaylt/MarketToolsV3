4182844fa3e62a4e8bfda55fe1b6f0c88976f213
Uladzislau Krupski <uladzislau.krupski@alreadymedia.com>
2025-02-03 19:11:20 +0300

    init: create abstract

---
+++ src/Identity.Application/Utilities/Sessions/Abstract/ISessionSearchBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Utilities.Sessions.Abstract
+{
+    internal interface ISessionSearchBuilder<TSession> where TSession : ISessionSearchModel
+    {
+        ISessionSearchBuilder<TSession> UseCache();
+        ISessionSearchBuilder<TSession> UseDatabase();
+        ISessionSearchBuilder<TSession> SetCacheTime(TimeSpan timeSpan);
+        Task<TSession> SearchAsync();
+    }
+}
