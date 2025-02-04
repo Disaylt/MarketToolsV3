c7773c2a07d35928fd75eca1257576298c3b1d9f
Disaylt <disofban@gmail.com>
2025-02-05 01:34:17 +0300

    init: create db context

---
+++ src/MarketToolsV3.FakeData.WebApi/Infrastructure/Database/FakeDataDbContext.cs
@@ -0,0 +1,13 @@
+﻿using MarketToolsV3.FakeData.WebApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketToolsV3.FakeData.WebApi.Infrastructure.Database
+{
+    public class FakeDataDbContext(DbContextOptions<FakeDataDbContext> options) : DbContext(options)
+    {
+        public DbSet<FakeDataTask> Tasks { get; set; } = null!;
+        public DbSet<ResponseBody> Responses { get; set; } = null!;
+        public DbSet<ValueUseHistory> ValueUseHistories { get; set; } = null!;
+        public DbSet<TaskDetails> TasksDetails { get; set; } = null!;
+    }
+}
