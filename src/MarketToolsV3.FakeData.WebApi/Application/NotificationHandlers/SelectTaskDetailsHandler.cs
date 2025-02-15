23095cc545cf047014e265b31dd23127318cb03b
Disaylt <disofban@gmail.com>
2025-02-16 00:56:50 +0300

    feat: implement notification handler

--- src/MarketToolsV3.FakeData.WebApi/Application/NotificationHandlers/SelectTaskDetailsHandler.cs
+++ src/MarketToolsV3.FakeData.WebApi/Application/NotificationHandlers/SelectTaskDetailsHandler.cs
@@ -1,20 +1,27 @@
 ﻿using MarketToolsV3.FakeData.WebApi.Application.Notifications;
 using MarketToolsV3.FakeData.WebApi.Application.Services.Abstract;
 using System;
+using MarketToolsV3.FakeData.WebApi.Domain.Entities;
 using MarketToolsV3.FakeData.WebApi.Domain.Seed;
 
 namespace MarketToolsV3.FakeData.WebApi.Application.NotificationHandlers
 {
     public class SelectTaskDetailsHandler(IPublisher<TimeoutTaskDetailsNotification> timeoutTaskDetailsNotificationPublisher,
-        IUnitOfWork unitOfWork)
+        IFakeDataDetailsEntityService fakeDataDetailsEntityService)
     : INotificationHandler<SelectTaskDetailsNotification>
     {
         public async Task HandleAsync(SelectTaskDetailsNotification notification)
         {
+            TaskDetails? taskDetails = await fakeDataDetailsEntityService.TakeNextAsync();
+
+            if (taskDetails is null)
+            {
+                return;
+            }
 
             TimeoutTaskDetailsNotification handleNotification = new()
             {
-                TaskDetailsId = randomId
+                TaskDetailsId = taskDetails.Id
             };
 
             await timeoutTaskDetailsNotificationPublisher.Notify(handleNotification);
