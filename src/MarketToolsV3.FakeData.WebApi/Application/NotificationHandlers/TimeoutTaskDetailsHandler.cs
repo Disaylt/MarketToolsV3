d8ace066bc857c2d3c76c331f05bc271582696cc
Disaylt <disofban@gmail.com>
2025-02-16 00:21:58 +0300

    feat: implement timeout handler

--- src/MarketToolsV3.FakeData.WebApi/Application/NotificationHandlers/TimeoutTaskDetailsHandler.cs
+++ src/MarketToolsV3.FakeData.WebApi/Application/NotificationHandlers/TimeoutTaskDetailsHandler.cs
@@ -1,20 +1,31 @@
 ﻿using MarketToolsV3.FakeData.WebApi.Application.Notifications;
 using MarketToolsV3.FakeData.WebApi.Application.Services.Abstract;
+using MarketToolsV3.FakeData.WebApi.Domain.Entities;
+using MarketToolsV3.FakeData.WebApi.Domain.Seed;
 
 namespace MarketToolsV3.FakeData.WebApi.Application.NotificationHandlers
 {
-    public class TimeoutTaskDetailsHandler(IPublisher<FakeDataTaskNotification> fakeDataTaskPublisher)
+    public class TimeoutTaskDetailsHandler(IPublisher<FakeDataTaskNotification> fakeDataTaskPublisher,
+        IRepository<TaskDetails> taskDetailsRepository)
     : INotificationHandler<TimeoutTaskDetailsNotification>
     {
         public async Task HandleAsync(TimeoutTaskDetailsNotification notification)
         {
-            int time = Random.Shared.Next(1, 10) * 1000;
+            TaskDetails taskDetails = await taskDetailsRepository.FindRequiredAsync(notification.TaskDetailsId);
 
-            await Task.Delay(time);
+            await AwaitTaskTimeout(taskDetails);
 
             FakeDataTaskNotification fakeDataTaskNotification = CreateNextNotification(1);
 
             await fakeDataTaskPublisher.Notify(fakeDataTaskNotification);
+        }
+
+        private static async Task AwaitTaskTimeout(TaskDetails taskDetails)
+        {
+            if (taskDetails.TimeoutBeforeRun > 0)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(taskDetails.TimeoutBeforeRun));
+            }
         }
 
         private static FakeDataTaskNotification CreateNextNotification(int taskId)
