1649f3cab903ec374779f2e1a0487aaa93c5796f
Disaylt <disofban@gmail.com>
2025-03-22 13:50:25 +0300

    add user command enrich user event

---
+++ src/WB.Seller.Companies.Processor/ServiceRegistrationExtension.cs
@@ -0,0 +1,37 @@
+﻿using MarketToolsV3.ConfigurationManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace WB.Seller.Companies.Processor
+{
+    internal static class ServiceRegistrationExtension
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection collection,
+            MessageBrokerConfig messageBrokerConfig,
+            string serviceName)
+        {
+            collection.AddMassTransit(mt =>
+            {
+
+                mt.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(messageBrokerConfig.RabbitMqConnection,
+                        "/",
+                        h =>
+                        {
+                            h.Username(messageBrokerConfig.RabbitMqLogin);
+                            h.Password(messageBrokerConfig.RabbitMqPassword);
+                        });
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            return collection;
+        }
+    }
+}
