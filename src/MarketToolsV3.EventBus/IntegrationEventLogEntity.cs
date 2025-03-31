28235d33fb2a21e5bf43a2680dca492d3f709929
Disaylt <disofban@gmail.com>
2025-04-01 02:07:59 +0300

    add outbox projects

---
+++ src/MarketToolsV3.EventBus/IntegrationEventLogEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using IntegrationEvents.Contract;
+
+namespace MarketToolsV3.EventBus
+{
+    public class IntegrationEventLogEntity
+    {
+        private static readonly JsonSerializerOptions IndentedOptions = new() { WriteIndented = true };
+
+        private IntegrationEventLogEntity(){}
+
+        public IntegrationEventLogEntity(BaseIntegrationEvent @event, Guid transactionId)
+        {
+            Content = JsonSerializer.Serialize(@event, @event.GetType(), IndentedOptions);
+            TransactionId = transactionId;
+            Id = @event.Id;
+            Type = @event.GetType().FullName 
+                   ?? throw new NullReferenceException("Event type is null");
+        }
+
+        public Guid Id { get; private set; }
+        public DateTimeOffset DateTime { get; private set; } = DateTimeOffset.UtcNow;
+        public EventStateEnum State { get; set; }
+        public int TimeSent { get; set; }
+        public Guid TransactionId { get; private set; }
+        public string Type { get; private set; }
+        public string Content { get; private set; }
+    }
+}
