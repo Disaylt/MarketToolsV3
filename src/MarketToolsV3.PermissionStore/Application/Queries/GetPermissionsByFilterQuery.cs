971f84377d16bd7db05bf8125d9dddf244c9175b
Disaylt <disofban@gmail.com>
2025-04-30 19:29:47 +0300

    add query for gettion module

---
+++ src/MarketToolsV3.PermissionStore/Application/Queries/GetPermissionsByFilterQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketToolsV3.PermissionStore.Application.Models;
+using MediatR;
+
+namespace MarketToolsV3.PermissionStore.Application.Queries;
+
+public class GetPermissionsByFilterQuery : IRequest<ModuleGroupDto>
+{
+    public required string Module { get; set; }
+}
---
+++ src/MarketToolsV3.PermissionStore/Application/Queries/GetPermissionsByFilterQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MarketToolsV3.PermissionStore.Application.Models;
+using MarketToolsV3.PermissionStore.Application.Services.Abstract;
+using MarketToolsV3.PermissionStore.Domain.Entities;
+using MediatR;
+
+namespace MarketToolsV3.PermissionStore.Application.Queries;
+
+public class GetPermissionsByFilterQueryHandler(
+    IPermissionsEntityService permissionsEntityService)
+    : IRequestHandler<GetPermissionsByFilterQuery, ModuleGroupDto>
+{
+    public async Task<ModuleGroupDto> Handle(GetPermissionsByFilterQuery request, CancellationToken cancellationToken)
+    {
+        IReadOnlyCollection<PermissionEntity> entities = await permissionsEntityService
+            .GetRangeByModuleAsync(request.Module, cancellationToken);
+
+        return new ModuleGroupDto
+        {
+            Module = request.Module,
+            Permissions = MapPermissions(entities)
+        };
+    }
+
+    private IReadOnlyCollection<ModulePermissionDto> MapPermissions(IEnumerable<PermissionEntity> entities)
+    {
+        return entities
+            .Select(x => new ModulePermissionDto()
+            {
+                Path = x.Path,
+                ViewName = x.ViewName
+            })
+            .ToList();
+    }
+}
