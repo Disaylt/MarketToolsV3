d3b981b973e892f0850e88e3ed605b8573e842d7
Disaylt <disofban@gmail.com>
2025-06-16 02:21:43 +0300

    implement permission client

--- src/WB.Seller.Features.Automation.PriceManager.Infrastructure/Services/Implementation/ExternalPermissionsService.cs
+++ src/WB.Seller.Features.Automation.PriceManager.Infrastructure/Services/Implementation/ExternalPermissionsService.cs
@@ -1,29 +1,57 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using MarketToolsV3.ConfigurationManager.Models;
 using Microsoft.Extensions.Options;
+using Proto.Contract.Common.PermissionStore;
+using WB.Seller.Features.Automation.PriceManager.Application.Seed;
 
 namespace WB.Seller.Features.Automation.PriceManager.Infrastructure.Services.Implementation;
 
-public class ExternalPermissionsService(
-    IOptions<ServicesAddressesConfig> addressesOptions)
+public class ExternalPermissionsService
     : IAsyncDisposable, IDisposable
 {
-    
+    private readonly GrpcChannel _grpcChannel;
+    private readonly ModuleConfig _permissionsConfig;
+    private readonly Permission.PermissionClient _permissionClient;
+
+    public ExternalPermissionsService(
+        IOptions<ServicesAddressesConfig> addressesOptions,
+        GrpcChannelOptions grpcChannelOptions)
+    {
+        _permissionsConfig = addressesOptions
+                        .Value
+                        .Common
+                        .Permissions;
+
+        string address = _permissionsConfig
+                         .Addresses
+                         .GetOrDefaultRandomGrpcAddress()
+                         ?? throw new NullReferenceException("Permissions grpc address not found.");
+
+        _grpcChannel = GrpcChannel.ForAddress(address, grpcChannelOptions);
+        _permissionClient = new Permission.PermissionClient(_grpcChannel);
+    }
+
     public async Task RefreshPermissionsAsync()
     {
-        GrpcChannelOptions channelOptions = new GrpcChannelOptions
+        RefreshRequest request = new RefreshRequest
         {
+            Permissions = { PermissionsStorage.Collection },
+            Module = _permissionsConfig.Name
+        };
 
-        }
+        await _permissionClient.RefreshAsync(request);
     }
 
     public ValueTask DisposeAsync()
     {
-        throw new NotImplementedException();
+        Dispose();
+
+        return ValueTask.CompletedTask;
     }
 
     public void Dispose()
     {
-        throw new NotImplementedException();
+        _grpcChannel.Dispose();
     }
 }
---
+++ src/WB.Seller.Features.Automation.PriceManager.Infrastructure/ServicesRegistrationExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using Grpc.Net.Client;
+
+namespace WB.Seller.Features.Automation.PriceManager.Infrastructure;
+
+public static class ServicesRegistrationExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddHttpClient("grpc");
+        serviceCollection.AddSingleton(serviceProvider => new GrpcChannelOptions
+        {
+            HttpClient = serviceProvider
+                .GetRequiredService<IHttpClientFactory>()
+                .CreateClient("grpc"),
+            DisposeHttpClient = false
+        });
+
+        return serviceCollection;
+    }
+}
