37f23639adf3083243dc5b72893151a108692a84
Disaylt <disofban@gmail.com>
2025-06-16 01:37:18 +0300

    fix config create grpc service

--- src/WB.Seller.Features.Automation.PriceManager.Infrastructure/Services/Implementation/ExternalPermissionsService.cs
+++ src/WB.Seller.Features.Automation.PriceManager.Infrastructure/Services/Implementation/ExternalPermissionsService.cs
@@ -1,9 +1,29 @@
-﻿namespace WB.Seller.Features.Automation.PriceManager.Infrastructure.Services.Implementation;
+﻿using Grpc.Net.Client;
+using MarketToolsV3.ConfigurationManager.Models;
+using Microsoft.Extensions.Options;
 
-public class ExternalPermissionsService
+namespace WB.Seller.Features.Automation.PriceManager.Infrastructure.Services.Implementation;
+
+public class ExternalPermissionsService(
+    IOptions<ServicesAddressesConfig> addressesOptions)
+    : IAsyncDisposable, IDisposable
 {
-    public async Task RefreshPermissions()
+    
+    public async Task RefreshPermissionsAsync()
     {
+        GrpcChannelOptions channelOptions = new GrpcChannelOptions
+        {
 
+        }
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
     }
 }
