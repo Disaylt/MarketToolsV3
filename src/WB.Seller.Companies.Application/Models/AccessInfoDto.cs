a99c1f21e92a8c328819dcf703fce624aff2ce66
Disaylt <disofban@gmail.com>
2025-07-02 11:10:40 +0300

    fix name

--- src/WB.Seller.Companies.Application/Models/AccessInfoDto.cs
+++ src/WB.Seller.Companies.Application/Models/AccessInfoDto.cs
@@ -5,5 +5,5 @@ namespace WB.Seller.Companies.Application.Models;
 public record AccessInfoDto
 {
     public required SubscriptionRole Role { get; init; }
-    public Dictionary<string, PermissionStatus> PermissionSettings { get; init; } = [];
+    public Dictionary<string, PermissionStatus> Permissions { get; init; } = [];
 }
