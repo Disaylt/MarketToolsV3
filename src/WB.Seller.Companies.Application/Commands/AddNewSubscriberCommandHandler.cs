647b49d7a67018b8ec0ecc7fbaeabfc66077db10
Disaylt <disofban@gmail.com>
2025-07-03 21:34:50 +0300

    implement command

--- src/WB.Seller.Companies.Application/Commands/AddNewSubscriberCommandHandler.cs
+++ src/WB.Seller.Companies.Application/Commands/AddNewSubscriberCommandHandler.cs
@@ -1,13 +1,17 @@
 ﻿using System.Net;
 using MediatR;
 using WB.Seller.Companies.Application.Services.Abstract;
+using WB.Seller.Companies.Application.Utilities.Abstract;
 using WB.Seller.Companies.Domain.Entities;
 using WB.Seller.Companies.Domain.Enums;
 using WB.Seller.Companies.Domain.Seed;
 
 namespace WB.Seller.Companies.Application.Commands;
 
-public class AddNewSubscriberCommandHandler(IUserEntityService userEntityService)
+public class AddNewSubscriberCommandHandler(IUserEntityService userEntityService,
+    ICodeUtility codeUtility,
+    IRepository<SubscriptionEntity> subscriptionRepository,
+    IUnitOfWork unitOfWork)
     : IRequestHandler<AddNewSubscriberCommand, Unit>
 {
     public async Task<Unit> Handle(AddNewSubscriberCommand request, CancellationToken cancellationToken)
@@ -16,8 +20,19 @@ public class AddNewSubscriberCommandHandler(IUserEntityService userEntityService
                    ?? throw new RootServiceException(HttpStatusCode.NotFound, "Пользователь с таким логином не найден.");
 
         SubscriptionEntity newSubscription = new(user.SubId, request.CompanyId, request.Note, SubscriptionRole.AwaitConfirmation);
+        SubscriptionCodeEntity subscriptionCode = CreateSubscriptionCode();
+        newSubscription.AddCode(subscriptionCode);
 
+        await subscriptionRepository.AddAsync(newSubscription, cancellationToken);
+        await unitOfWork.SaveEntitiesAsync(cancellationToken);
 
-        throw new NotImplementedException();
+        return Unit.Value;
+    }
+
+    private SubscriptionCodeEntity CreateSubscriptionCode()
+    {
+        string code = codeUtility.Generate();
+
+        return new(code);
     }
 }
